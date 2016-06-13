bd4b95cffa21663d0620e6b0fe3e5a3582ed5a1b
Matthew Phillips <matthew@matthewphillips.info>
2016-06-13 11:43:35 -0400

    Remove old grunt-steal

--- test/fixtures/build.js
+++
@@ -1,8 +0,0 @@
-load('steal/rhino/rhino.js');
-
-steal('steal/build','steal/build/scripts','steal/build/styles', function() {
-	steal.build('../test/fixtures/index.html', {
-		to: '../tmp',
-		compressor: 'uglify'
-	});
-});
